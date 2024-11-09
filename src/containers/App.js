4317478fc05cf5de401237307112c3a16f1dbd78
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-11-09 14:54:35 -0500

    added error handling

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from "../components/CardList";
 // import { robots } from "./robots";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 class App extends Component {
     constructor() {
@@ -39,7 +40,9 @@ class App extends Component {
                 <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <Scroll>
-                    <CardList robots={filteredRobots} />
+                    <ErrorBoundary>
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundary>
                 </Scroll>
             </div>
         );
